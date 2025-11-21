8f8c83b5e6af2e22078017c8dd737f2a3d0aee7a
sirstvie <emendi23@ku.edu.tr>
2025-11-22 01:07:43 +0300

    WIP: local changes

--- app/src/main/java/kuroyale/mainpack/cards/Card.java
+++ app/src/main/java/kuroyale/mainpack/cards/Card.java
@@ -1,6 +1,6 @@
 package kuroyale.mainpack.cards;
 
-public class Card {
+public abstract class Card {
 
     private int id;
     private String name;
@@ -11,11 +11,46 @@ public class Card {
     private CardTarget target;
     
 
-   public Card(String name, int cost, CardType type, CardCategory category, CardTarget target) {
+   public Card(int id, String name, String description, int cost, CardType type, CardCategory category, CardTarget target) {
+        this.id = id;
         this.name = name;
+        this.description = description;
         this.cost = cost;
         this.type = type;
         this.category = category;
         this.target = target;
+    }
+
+       public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public CardType getType() {
+        return type;
+    }
+
+    public CardCategory getCategory() {
+        return category;
+    }
+
+    public CardTarget getTarget() {
+        return target;
+    }
+
+        public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Cost: " + cost + ")" + "\n" + "Description: " + description;
     }
 }
---
+++ app/src/main/java/kuroyale/mainpack/cards/subclasses/UnitCard.java
@@ -0,0 +1,3 @@
+package kuroyale.mainpack.cards.subclasses;
+
+
