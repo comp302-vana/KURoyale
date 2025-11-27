435095a7d12b21f766aa096804b2030b08fdfe46
ttoklucu23 <ttoklucu23@ku.edu.tr>
2025-11-27 22:45:09 +0300

    Card shuffle and put the played card at the bottom

---
+++ app/src/main/java/kuroyale/mainpack/InGameCardController.java
@@ -0,0 +1,16 @@
+package kuroyale.mainpack;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class InGameCardController {
+    public static <T> void shuffle(List<T> deck){
+        Collections.shuffle(deck);
+    }
+    public static <T> void moveCardToBottom(List<T> deck,int index) {
+        T card = deck.remove(index);
+        deck.add(card);
+    }
+
+}
