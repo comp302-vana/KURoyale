7170a6ddd15ac9527771444e134dd05e1057aabb
funal23 <funal23@ku.edu.tr>
2026-01-11 10:39:59 +0300

    Building Spawn Logic Added

--- app/src/main/java/kuroyale/entitiypack/subclasses/BuildingEntity.java
+++ app/src/main/java/kuroyale/entitiypack/subclasses/BuildingEntity.java
@@ -6,6 +6,11 @@ public class BuildingEntity extends AliveEntity {
     private final BuildingCard card;
     private final double initialHealth;
 
+    private double timeSinceLastSpawn = 0.0;
+    private double timeSinceLastElixirGeneration = 0.0;
+    private int elixirGenerated = 0;
+
+
     public BuildingEntity(BuildingCard card, boolean isPlayer) {
         super(card, isPlayer);
         this.card = card;
@@ -23,5 +28,41 @@ public class BuildingEntity extends AliveEntity {
 
     protected double getLifetime() {
         return card.getLifetime();
+    }
+
+    public double getTimeSinceLastSpawn() {
+        return timeSinceLastSpawn;
+    }
+
+    public void addTimeSinceLastSpawn(double deltaTime){
+        timeSinceLastSpawn += deltaTime;
+    }
+
+    public void resetTimeSinceLastSpawn() {
+        timeSinceLastSpawn = 0.0;
+    }
+    
+    public double getTimeSinceLastElixirGeneration() {
+        return timeSinceLastElixirGeneration;
+    }
+    
+    public void addTimeSinceLastElixirGeneration(double deltaTime) {
+        timeSinceLastElixirGeneration += deltaTime;
+    }
+    
+    public void resetTimeSinceLastElixirGeneration() {
+        timeSinceLastElixirGeneration = 0.0;
+    }
+    
+    public int getElixirGenerated() {
+        return elixirGenerated;
+    }
+    
+    public void incrementElixirGenerated() {
+        elixirGenerated++;
+    }
+    
+    public BuildingCard getBuildingCard() {
+        return card;
     }
 }
